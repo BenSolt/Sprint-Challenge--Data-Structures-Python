9b89b3799959b44cf4d100b6230dd857a34177a3
benjamin-solt <bensolt@gmail.com>
2020-05-15 12:11:14 -0400

    working on ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,33 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.currrent = None
+
 
     def append(self, item):
-        pass
+        if len() == 0:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.add_to_tail
+        #else if length of storage is full
+        elif len(self.storage) == self.capacity:
+            #if current is equal to end
+            if self.current == self.storage.tail:
+                #remove end
+                self.storage.remove_from_tail()
+                #add end
+                self.storage.add_to_tail(item)
 
     def get(self):
-        pass
+#############################################  
+        list_buffer = []
+
+        start = self.storage.head
+        while True:
+            list_buffer .append(start.value)
+            if start.next == None:
+            #end program
+                break
+            start = start.next
+
+            print(list_buffer)
+            return list_buffer
