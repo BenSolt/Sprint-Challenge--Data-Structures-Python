496adcebf459b832b8b0a4d18e80df8086d7f06a
benjamin-solt <bensolt@gmail.com>
2020-05-15 13:47:48 -0400

    got mvp

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,33 +1,61 @@
+from doubly_linked_list import DoublyLinkedList
+
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.currrent = None
-
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-        if len() == 0:
+         #if length of storage equals 0
+        if len(self.storage) == 0:
+            #add item to end
             self.storage.add_to_tail(item)
-            self.current = self.storage.add_to_tail
-        #else if length of storage is full
+            #current, None = end
+            self.current = self.storage.tail
+        #if length of storage is full.
         elif len(self.storage) == self.capacity:
-            #if current is equal to end
+           #if current is equal to end
             if self.current == self.storage.tail:
-                #remove end
+                #remove tail
                 self.storage.remove_from_tail()
-                #add end
+                #add to tail
                 self.storage.add_to_tail(item)
+                #makes current the beginning
+                self.current = self.storage.head
+                return
+            
+            oldest = self.current
+            #current = current plus 1, (spot next to).
+            self.current = self.current.next
+            #removes oldest current
+            self.storage.delete(oldest)
+            #adds item to end
+            self.storage.add_to_tail(item)
+           #variable = end
+            addedNode = self.storage.tail
+            
+            self.current = self.current.next
+           
+            while self.current.prev is not addedNode:
+                self.storage.move_to_end(self.current.prev)
+                self.current = self.current.next
+        else:
+            self.storage.add_to_tail(item)
+
 
     def get(self):
 #############################################  
         list_buffer = []
-
-        start = self.storage.head
+       
+        Begin = self.storage.head
         while True:
-            list_buffer .append(start.value)
-            if start.next == None:
-            #end program
-                break
-            start = start.next
+            list_buffer.append(Begin.value)
+            if Begin.next == None:
+                #Quit Program
+              break
+            Begin = Begin.next
+            
+        print(list_buffer)
 
-            print(list_buffer)
-            return list_buffer
+        return list_buffer
